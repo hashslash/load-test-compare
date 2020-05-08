96eef61e7783be9db3d5dc190edf7e5c79464cc5
SushanthKille <skille@mscripts.com>
2020-05-08 19:44:22 +0530

    expressjs

---
+++ expressjs/app.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const port = 8000
+app.get('/', (req, res) => res.sendFile('./public/index.html'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
